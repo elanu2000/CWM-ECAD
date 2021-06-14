e68bdc252051762ebcad8ede2fd94c09aea27b4f
Cloud User <centos@ip-172-31-12-1.eu-west-2.compute.internal>
2021-06-14 15:30:00 +0000

    A solution to exercise 2

--- Ex2/top.v
+++ Ex2/top.v
@@ -21,12 +21,17 @@
 `timescale 1ns / 100ps
 
 module doorbell(
+	input a,
+	input b, 
+	input sel,
+	output out
     //Todo: define inputs here
     );
     
     //Todo: define registers and wires here
-
+	wire out;
 
     //Todo: define your logic here                 
-      
+      assign #5 out = (sel) ? b : a; //if sel is 1 assign out to a 
+//else sel is 0 assign out to b
 endmodule
