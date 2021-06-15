5f3b511782e265525d032796f8b945803161a344
Cloud User <centos@ip-172-31-12-1.eu-west-2.compute.internal>
2021-06-15 14:16:51 +0000

    An attempt to exercise 4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Mihnea Constantin
+// Date: 15/06/21
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -16,3 +16,21 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module led(
+	input rst, clk, button,
+	output reg [2:0] colour
+	);
+always @(posedge button) begin
+	case(colour)
+          3'b001: colour = 3'b010;
+          3'b010: colour = 3'b011;
+          3'b011: colour = 3'b100;
+          3'b100: colour = 3'b101;
+          3'b101: colour = 3'b110;
+          3'b110: colour = 3'b001;
+          default: colour = 3'b001;
+      endcase
+    end     
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,67 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Mihnea Constantin
+// Date: 15/06/21
 //
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+module top_tb(
 
+	);
+
+parameter CLK_PERIOD = 10;
+
+reg rst, clk, button, error;
+wire [2:0] colour;
+reg [2:0] prev;
+
+initial begin
+	clk= 1'b0;
+	forever 
+		#(CLK_PERIOD/2) clk = ~clk;
+end
+
+initial begin
+	rst = 1;
+	button = 0;
+	error = 0;
+	#(2 * CLK_PERIOD)
+	if (colour != 3'b001) begin
+		$display("TEST FAILED with reset!");
+		error = 1;
+	end
+prev = colour;
+rst = 0;
+#(5 * CLK_PERIOD)
+if(colour != prev) begin
+	$display("Test FAILED with change");
+	error = 1;
+end
+button = 1;
+forever begin
+	prev = colour;
+	#(CLK_PERIOD)
+	if (prev == 3'b110) begin
+		if (colour != 3'b001) begin
+			$display("Test FAILED with change");
+			error = 1;
+		end
+	end
+	else begin
+		if (colour != (prev + 1)) begin
+			$display("Test FAILED with change");
+                        error =	1;
+		end
+	end
+end
+end
+initial begin
+	#(50 * CLK_PERIOD)
+	if(error == 0)
+		$display("***TEST PASSED! :) ***");
+	$finish;
+end
+
+led top(.rst(rst), .clk(clk), .button(button), .colour(colour));
+endmodule
