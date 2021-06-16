a09cbcc8a6b88aabc7d9340884891deb90ecec65
Cloud User <centos@ip-172-31-12-1.eu-west-2.compute.internal>
2021-06-16 15:54:26 +0000

    progress on exercise 6

--- Ex6/project.tcl
+++ Ex6/project.tcl
@@ -19,7 +19,8 @@ puts "Creating Project"
 create_fileset -constrset -quiet constraints
 #add_files -fileset constraints -norecurse ${project_constraints}
 #set_property is_enabled false [get_files ${project_constraints}]
-
+create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name blk_mem_gen_1
+set_property -dict [list CONFIG.Write_Width_A {24} CONFIG.Write_Depth_A {8} CONFIG.Read_Width_A {24} CONFIG.Write_Width_B {24} CONFIG.Read_Width_B {24}] [get_ips blk_mem_gen_1]
 #Todo: add verilog modules here
 read_verilog "top.v"
 read_verilog "top_tb.v"
--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Mihnea Constantin
+// Date: 16/06/21
 //
 //
 //  Description: In this exercise, you need to design a memory with 8 entries, 
@@ -15,3 +15,20 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module rgb(
+	input clk,
+	input [2:0] colour,
+	input enable,
+	output [23:0] rgb
+	);
+blk_mem_gen_1 your_instance_name (
+  .clka(clka),    // input wire clka
+  .ena(ena),      // input wire ena
+  .wea(wea),      // input wire [0 : 0] wea
+  .addra(addra),  // input wire [2 : 0] addra
+  .dina(dina),    // input wire [23 : 0] dina
+  .douta(douta)  // output wire [23 : 0] douta
+);
+endmodule
