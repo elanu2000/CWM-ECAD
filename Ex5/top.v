ddd351d98013ea44e0476b9ef2e184424c654f53
Cloud User <centos@ip-172-31-12-1.eu-west-2.compute.internal>
2021-06-16 10:17:06 +0000

    Progress on exercise 5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Mihnea Constantin
+// Date: 16/06/21
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -12,4 +12,36 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module air(
+	input clk,
+	input [4:0] temperature,
+	output reg heating, cooling
+	);
+always @(posedge clk) begin
+	case ({heating, cooling})
+		2'b00: begin
+			if (temperature >= 5'b11000)
+				cooling = 1'b1;
+			else
+			if (temperature <= 5'b10010)
+				heating = 1'b1;
+			end
+		2'b01: begin
+			if (temperature <= 5'b10100)
+				cooling = 1'b0;
+			end
+		2'b10: begin
+			if (temperature >= 5'b10100)
+				heating = 1'b0;
+			end
+		default: begin
+			heating = 0;
+			cooling = 0;
+			end
+	endcase
+end
+endmodule
+				
+		
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,81 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Mihnea Constantin
+// Date: 16/06/21
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+module top_tb(
 
+	);
+
+parameter CLK_PERIOD = 10;
+reg clk, error;
+reg [4:0] temperature;
+wire heating,cooling;
+reg [1:0] prev;
+initial begin
+	clk= 1'b0;
+	forever 
+		#(CLK_PERIOD/2) clk = ~clk;
+end
+
+
+initial begin
+	temperature = 5'b10100;
+	error = 0;
+	#(2 * CLK_PERIOD)
+	if({heating, cooling} != 2'b00) begin
+		$display("TEST FAILED initially with 20!");
+		error = 1;
+	end
+	temperature = 5'b10011;
+        #(CLK_PERIOD)
+        if({heating, cooling} != 2'b00) begin
+                $display("TEST FAILED initially with 19!");
+                error = 1;
+        end
+	temperature = 5'b10101;
+        #(CLK_PERIOD)
+        if({heating, cooling} != 2'b00) begin
+                $display("TEST FAILED initially with 21!");
+                error = 1;
+        end
+	temperature = 5'b10000;
+        #(CLK_PERIOD)
+        if({heating, cooling} != 2'b10) begin
+                $display("TEST FAILED idle state at 16!");
+                error = 1;
+        end
+	temperature = 5'b10100;
+        #(CLK_PERIOD)
+        if({heating, cooling} != 2'b00) begin
+                $display("TEST FAILED heating state at 20!");
+                error = 1;
+        end                 
+        temperature = 5'b11000;
+        #(CLK_PERIOD)
+        if({heating, cooling} != 2'b01) begin
+                $display("TEST FAILED idle state at 24!");
+                error = 1;
+        end
+	temperature = 5'b10100;
+        #(CLK_PERIOD)
+        if({heating, cooling} != 2'b00) begin
+                $display("TEST FAILED cooling state at 20!");
+                error = 1;
+        end
+temperature = 5'b10000;       
+forever begin
+	
+end
+initial begin
+	#(50 * CLK_PERIOD)
+	if(error == 0)
+		$display("***TEST PASSED! :) ***");
+	$finish;
+end
+air top(.clk(clk), .temperature(temperature), .heating(heating), .cooling(cooling));
+endmodule		
+	
